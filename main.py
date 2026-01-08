3e1227d7184042cce769ad0e082fb3533a0048fd
TJ_Gaba <tjgaba@outlook.com>
2026-01-08 10:50:49 +0200

    Just finished assessment by double checking the main.py code

--- main.py
+++ main.py
@@ -22,3 +22,5 @@ if __name__ == "__main__":
     print("Calculator v1.2.0")
     print(f"10 + 5 = {add(10, 5)}")
     print(f"10 - 5 = {subtract(10, 5)}")
+    print(f"10 * 5 = {multiply(10, 5)}")
+    print(f"10 / 5 = {divide(10, 5)}")
